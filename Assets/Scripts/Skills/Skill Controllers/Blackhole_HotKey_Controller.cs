f4976330c4c19d6c4eba6e78613586ac3591c166
LukasDebugGitHub <lukas.solo28@gmail.com>
2024-09-16 21:33:24 +0200

    Section 11, part 1 2/2

--- Assets/Scripts/Skills/Skill Controllers/Blackhole_HotKey_Controller.cs
+++ Assets/Scripts/Skills/Skill Controllers/Blackhole_HotKey_Controller.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Blackhole_HotKey_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer sr;
+    private KeyCode myHotKey;
+    private TextMeshProUGUI myText;
+
+    private Transform myEnemy;
+    private Blackhole_Skill_Controller blackHole;
+
+    public void SetupHotKey(KeyCode _myNewHotKey, Transform _myEnemy, Blackhole_Skill_Controller _myBlackHole)
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        myText = GetComponentInChildren<TextMeshProUGUI>();
+
+        myEnemy = _myEnemy;
+        blackHole = _myBlackHole;
+
+        myHotKey = _myNewHotKey;
+        myText.text = _myNewHotKey.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(myHotKey))
+        {
+            blackHole.AddEnemyToList(myEnemy);
+
+            myText.color = Color.clear;
+            sr.color = Color.clear;
+        }
     }
 }
--- Assets/Scripts/Skills/Skill Controllers/Blackhole_Skill_Controller.cs
+++ Assets/Scripts/Skills/Skill Controllers/Blackhole_Skill_Controller.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Blackhole_Skill_Controller : MonoBehaviour
 {
+    [SerializeField] private GameObject hotKeyPrefab;
+    [SerializeField] private List<KeyCode> keyCodeList;
+
     public float maxSize;
     public float growSpeed;
     public bool canGrow;
 
-    public List<Transform> targets;
+    private List<Transform> targets = new List<Transform>();
 
     private void Update()
     {
@@ -20,11 +23,31 @@ public class Blackhole_Skill_Controller : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<Enemy>()  != null)
+        if (collision.GetComponent<Enemy>() != null)
         {
             collision.GetComponent<Enemy>().FreezeTime(true);
 
-            //targets.Add(collision.transform);
+            CreateHotkey(collision);
         }
     }
+
+    private void CreateHotkey(Collider2D collision)
+    {
+        if (keyCodeList.Count <= 0)
+        {
+            Debug.LogWarning("Not enough hot keys in a key code list!");
+            return;
+        }
+
+        GameObject newHotKey = Instantiate(hotKeyPrefab, collision.transform.position + new Vector3(0, 2), Quaternion.identity);
+
+        KeyCode choosenKey = keyCodeList[Random.Range(0, keyCodeList.Count)];
+        keyCodeList.Remove(choosenKey);
+
+        Blackhole_HotKey_Controller newHotKeyScript = newHotKey.GetComponent<Blackhole_HotKey_Controller>();
+
+        newHotKeyScript.SetupHotKey(choosenKey, collision.transform, this);
+    }
+
+    public void AddEnemyToList(Transform _enemyTransform) => targets.Add(_enemyTransform);
 }
